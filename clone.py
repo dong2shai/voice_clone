cf78ee7df53b1583e66d2a1ec5b159abd692d445
Shenghai Dong <18366182863@163.com>
2023-02-20 17:08:18 +0800

    提交声音处理代码

--- clone.py
+++ clone.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 
-
-
+import os
 import argparse
-
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='voice clone tool')
 
-    parser.add_argument('--source-file-path', type=str, 
-					help='Path of voice', required=True)
+    parser.add_argument('--phoneme', type=str, 
+                                    help='The file of voice as phoneme', required=True)
+
+    parser.add_argument('--base-band',type=str, 
+                                    help='The file of voice as base band', required=True)
+
+    parser.add_argument('--output', type=str,
+                                    help='The path of result voice tools handled ', required=False)
 
     args = parser.parse_args()
+
+    if not os.path.isfile(args.phoneme):
+        raise Exception("The phoneme file not exists")
+
+    if not os.path.isfile(args.base_band):
+        raise Exception("The base band file not exists")
+
+
+
+
+
--- src/voice/voice.py
+++ src/voice/voice.py
@@ -1,11 +1,59 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-def 
+import librosa
+import os 
+import sys
+import numpy as np
 
+from pydub import AudioSegment
+from scipy.io import wavfile
+
+def mp3_to_wav(mp3_voice, wav_voice):
+    convert =  AudioSegment.from_mp3(mp3_voice)
+    covert.export(destin, format='wav')
+
+def voice_duration(voice):
+    return librosa.get_duration(voice)
+
+def voice_rate(voice):
+    y, sr = librosa(voice, sr=None)
+    return sr
+
+def compare_duration(voice1, voice2):
+    d1 = voice_duration(voice1)
+    d2 = voice_duration(voice2)
+
+    return d1 == d2 
+
+
+def  reset_sampling_rate(voice, rate, path):
+    y, sr = librosa(voice, sr=None)
+    y_rt = librosa.resample(y, sr, rate)
+
+    librosa.output.write_wav(path, y_rt, rate)
+
+
+def detach_voice_chan(voice, left, right):
+    left_voice = []
+    right_voice = []
+
+    try:
+        rate, wav  = wavfile.read(voice)
+        for item in wav :
+            left_voice.append(item[0])
+            right_voice.append(item[1])
+
+        wavfile.write(left, rate, np.array(left_voice))
+        wavfile.write(right, rate, np.array(right_voice))
+
+    except IOError as e:
+        print('datach voice channel error %s' % str(e))
+    except:
+        print('Unkonw error', sys.exec_info())
 
 
 
 
 if __name__ == '__main__':
-    pass 
+    pass
